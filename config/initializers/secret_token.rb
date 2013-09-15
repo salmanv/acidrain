bcbfedcb47a306896e353f8adfeab8666ffd8260
Salman Vajid <s@vajid.com>
2013-09-15 18:23:24 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acidrain::Application.config.secret_key_base = 'b988bb61a3ed434ec8cf697643f68e67dcd799c5fcde1193b264fea8b7263e98bbe25fbc96832ae7d0453f2e972ac0fa4fa40d797ef26b17da18acb461ce3e9f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Acidrain::Application.config.session_store :cookie_store, key: '_acidrain_session'

4f7993afa5fadd21d4037fc7fc00ae44c07b2e7b
Salman Vajid <s@vajid.com>
2013-09-15 20:36:31 -0400

    Refactoring the products controller to respond to JSON

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,29 @@
 class ProductsController < ApplicationController
 	def index
 		@products = Product.all
+
+		respond_to do |format|
+			format.html
+			format.json {render json: @products}
+		end
 	end
 
 	def show
 		@products = Product.find (params[:id])
+
+		respond_to do |format|
+			format.html
+			format.json {render json: @products}
+		end
 	end
 
 	def new
 		@products = Product.new
+
+		respond_to do |format|
+			format.html
+			format.json {render json: @products}
+		end
 	end
 
 	def edit
@@ -17,25 +32,39 @@ class ProductsController < ApplicationController
 
 	def create
 		@products = Product.new(params[:product])
+		respond_to do |format| 
 		if @product.save
-			redirect_to product_url
+			format.html {redirect_to @product, notice: 'Product was successfully created!'}
+			format.json {render json: @product, status: :created, location: @product}
 		else
-			render :new
+			format.html {render action: "new"}
+			format.json {render json: @product.errors, status: :unprocessable_entity}
+			end
 		end
 	end
 
 	def update
 		@products = product.find (params[:id])
-		if @product.update_attributes(params[:product])
-			redirect_to product_path(@product) #ask about this step** why @product
+
+		respond_to do |format|
+			if @product.update_attributes(params[:product])
+			format.html {redirect_to @product, notice: 'Product was successfully updated'}
+			format.json {head :no_content}
 		else
-			render :edit
+			format.html {render action: "edit"}
+			format.json {render json: @product.errors, status: :unprocessable_entity}
+			redirect_to product_path(@product) #ask about this step** why @product
+			end
 		end
 	end
 
 	def destroy
 		@product = product.find (params[:id])
 		@product.destroy
-		redirect_to product_path
+		
+		respond_to do |format|
+			format.html {redirect_to products_url}
+			format.json {head :no_content}
+		end
 	end
 end

676b1ba618e038c441285ecf02b96b286fe5730b
Salman Vajid <s@vajid.com>
2013-09-16 22:06:40 -0400

    created log in funcitonality and flash alerts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Acidrain::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   get "users/new"
   get "users/create"
  resources :products
  resources :users, :only => [:new, :create]
+ resources :sessions, :only => [:new, :create, :destroy]
  
 
 

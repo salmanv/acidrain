5a900724e0165af168a3d46a4dd833c0eaeccea7
Salman Vajid <s@vajid.com>
2013-09-18 09:20:52 -0400

    Adding user names, need to fix shit though

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
-
+	# attr_accessible :email, :password, :password_confirmation, :name
+	has_many :reviews
 	has_many :products, :through => :reviews
 
+	validates_presence_of :name
+	
 	has_secure_password
 	validates :email, :presence => true 
+	
 	validates_presence_of :password, :on => :create
 end
